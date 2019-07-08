562ca83a6de534a85dfee17b667ea010ffed4278
Moises VelasqueZ <mvelasquez@pictoric.com>
2019-07-08 15:15:07 -0500

    checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  message = 'Curso de angular';
+  message = 'Curso de angular...!';
 }
