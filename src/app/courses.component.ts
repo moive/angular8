5a7303e73b714d3c5e9f597e497d391943331976
Moises Velasquez <mvelasquez@pictoric.com>
2019-07-08 16:08:27 -0500

    create component in a classic way

---
+++ src/app/courses.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'courses',
+    template:`
+        <h1>My Courses</h1>
+        <p>TypeScript</p>
+    `,
+    styles:['h1{color:orange;}']
+})
+
+export class CoursesComponent {
+
+}
